125a2599605799aaecb0fb3d0f4adaca37265265
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-07-09 05:06:34 +0900

    0189_RotateArray.pyの修正

--- 0189_RotateArray.py
+++ 0189_RotateArray.py
@@ -1,22 +1,27 @@
-def rotate(nums, k):
-    """
-    Do not return anything, modify nums in-place instead.
-     """
-    n = len(nums)
-    k = k % n
-
-    reverse(nums, 0, n-1)
-    reverse(nums, 0, k-1)
-    reverse(nums, k, n-1)
+from typing import List
 
 
-def reverse(nums, start, end):
-    while start < end:
-        nums[start], nums[end] = nums[end], nums[start]
-        start += 1
-        end -= 1
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k = k % n
+
+        self.reverse(nums, 0, n - 1)
+        self.reverse(nums, 0, k - 1)
+        self.reverse(nums, k, n - 1)
+
+    def reverse(self, nums: List[int], start: int, end: int) -> None:
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
 
 
+# テストケース
+solution = Solution()
 nums = [1, 2, 3, 4, 5, 6, 7]
-rotate(nums, 3)
+solution.rotate(nums, 3)
 print(nums)
