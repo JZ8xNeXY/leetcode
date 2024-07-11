769772bb87157234e88b56f776200d7358373b8f
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-07-12 04:30:53 +0900

    0014_longestcommonprefix2.py

--- 0014_longestcommonprefix.py
+++ 0014_longestcommonprefix.py
@@ -1,14 +1,19 @@
+from typing import List
+
+
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         if not strs:
             return ''
 
-        prefix = strs[0]
+        for i in range(len(strs[0])):
+            char = strs[0][i]
+            for string in strs[1:]:
+                if i >= len(string) or string[i] != char:
+                    return strs[0][:i]
 
-        for string in strs[1:]:
-            while not string.startswith(prefix):
-                prefix = prefix[:-1]
-                if not prefix:
-                    return ''
+        return strs[0]
 
-        return prefix
+
+solution = Solution()
+print(solution.longestCommonPrefix(["flower", "flow", "flight"]))
