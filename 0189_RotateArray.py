ef07e4a7594a8edbf51599a49ed7b60d8067a51c
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-08-27 05:00:18 +0900

    0189_RotateArray.pyの修正

--- 0189_RotateArray.py
+++ 0189_RotateArray.py
@@ -13,7 +13,7 @@ class Solution:
         self.reverse(nums, 0, k-1)
         self.reverse(nums, k, n-1)
 
-    def reverse(self, nums: List[int], start: int, end: int) -> List:
+    def reverse(self, nums: List[int], start: int, end: int) -> None:
         while start < end:
             nums[start], nums[end] = nums[end], nums[start]
             start += 1
