c75de07531d7e0f2248b427298e33e14a26fe10e
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-08-28 05:39:28 +0900

    0028_FindtheIndexoftheFirstOccurrenceinaString.pyの修正

---
+++ 0028_FindtheIndexoftheFirstOccurrenceinaString.py
@@ -0,0 +1,21 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if not needle:
+            return -1
+
+        if len(needle) > len(haystack):
+            return -1
+
+        needle_length = len(needle)
+
+        for index in range(len(haystack)-needle_length+1):
+            if needle == haystack[index:index + needle_length]:
+                return index
+
+        return -1
+
+
+haystack = "leetcode"
+needle = "leeto"
+solution = Solution()
+solution.strStr(haystack, needle)
