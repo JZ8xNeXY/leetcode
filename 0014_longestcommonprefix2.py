c2866f7653da9a58434b6810bc29891fab42b11f
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-08-28 11:16:07 +0900

    0014_longestcommonprefix2.pyの修正

--- 0014_longestcommonprefix2.py
+++ 0014_longestcommonprefix2.py
@@ -1,29 +1,32 @@
 from typing import List
 
-# 分割統治法
-
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         if not strs:
             return ""
 
-        return self._longestCommonPrefix(strs, 0, len(strs)-1)
+        return self._longestCommonPrefix(strs, 0, len(strs) - 1)
 
     def _longestCommonPrefix(self, strs: List[str], left: int, right: int) -> str:
         if left == right:
             return strs[left]
 
         mid = (left + right) // 2
-        lcpLeft = self._commonPrefix(strs, left, mid)
-        lcpRight = self._commonPrefix(strs, mid+1, right)
+        lcpLeft = self._longestCommonPrefix(strs, left, mid)
+        lcpRight = self._longestCommonPrefix(strs, mid + 1, right)
 
         return self._commonPrefix(lcpLeft, lcpRight)
 
     def _commonPrefix(self, left: str, right: str) -> str:
-        minLength = min(len(left), min(right))
-        for i in minLength:
+        minLength = min(len(left), len(right))
+        for i in range(minLength):
             if left[i] != right[i]:
                 return left[:i]
 
         return left[:minLength]
+
+
+strs = ["flower", "flow", "flight"]
+solution = Solution()
+print(solution.longestCommonPrefix(strs))  # 出力: "fl"
