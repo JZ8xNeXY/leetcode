0eedf7ded3e0d92f27b8b37c8664d412cf57b936
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-08-28 10:35:03 +0900

    0008_StringtoInteger.pyの修正

--- 0008_StringtoInteger.py
+++ 0008_StringtoInteger.py
@@ -1,29 +1,33 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
         i = 0
-        while i < len(s) and s[i] == ' ':
-            i += 1
-
         sign = 1
-        while i < len(s) and s[i] in ('-', '+'):
-            sign = -1 if s[i] == '-' else 1
-            i += 1
-
         num = 0
-        while i < len(s) and s[i].isdigit():
-            num = num*10 + int(s[i])
-            i += 1
-
         INT_MAX = 2**31 - 1
         INT_MIN = -2**31
 
+        while i < len(s) and s[i] == " ":
+            i += 1
+
+        if i < len(s) and s[i] in ('-', '+'):
+            sign = -1 if s[i] == '-' else 1
+            i += 1
+
+        while i < len(s) and s[i].isdigit():
+            num = num * 10 + int(s[i])
+            i += 1
+
+        num *= sign
+
         if num > INT_MAX:
             return INT_MAX
-        if num < INT_MIN:
+        elif num < INT_MIN:
             return INT_MIN
 
         return num
 
 
+# 使用例
+s = "words and 987"
 solution = Solution()
-print(solution.myAtoi("                  42"))
+print(solution.myAtoi(s))
