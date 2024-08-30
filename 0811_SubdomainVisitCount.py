700561fec0b05bcf1403d4ed4b9088f5f1ad268d
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-08-31 04:36:29 +0900

    0811_SubdomainVisitCount.py

---
+++ 0811_SubdomainVisitCount.py
@@ -0,0 +1,27 @@
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+        count_dict = defaultdict(int)
+
+        for domain_info in cpdomains:
+            count, domain = domain_info.split(' ')
+            count = int(count)
+
+            subdomains = domain.split('.')
+            print(subdomains)
+            for i in range(len(subdomains)):
+                subdomain = '.'.join(subdomains[i:])
+                count_dict[subdomain] += count
+
+        result = [f"{cnt} {dom}" for dom, cnt in count_dict.items()]
+        print(result)
+        return result
+
+
+cpdomains = ["900 google.mail.com", "50 yahoo.com",
+             "1 intel.mail.com", "5 wiki.org"]
+solution = Solution()
+solution.subdomainVisits(cpdomains)
