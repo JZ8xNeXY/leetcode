30cf25f0844a667cb1681f90bb306665bc78bc26
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-09-07 17:55:51 +0900

    0070_ClimbinStairs.py

---
+++ 0070_ClimbinStairs.py
@@ -0,0 +1,20 @@
+class Solution:
+    def climbStairs(self, n: int, memo={}) -> int:
+        if n in memo:
+            return memo[n]
+
+        if n == 1:
+            return 1
+
+        if n == 2:
+            return 2
+
+        memo[n] = self.climbStairs(n-1, memo) + self.climbStairs(n-2, memo)
+
+        print(memo)
+        return memo[n]
+
+
+solution = Solution()
+
+solution.climbStairs(5)
