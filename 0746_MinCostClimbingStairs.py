053baab2223c438783333a6c6773778601b3fa6a
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-09-09 05:06:00 +0900

    0100_SameTree.py

--- 0746_MinCostClimbingStairs.py
+++ 0746_MinCostClimbingStairs.py
@@ -6,10 +6,11 @@ class Solution:
         n = len(cost)
         dp = [0] * (n + 1)
 
-        for i in range(2, n + 1):
+        for i in range(2, n+1):
             dp[i] = min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2])
+
         print(dp)
-        return dp[n]
+        return dp
 
 
 cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
