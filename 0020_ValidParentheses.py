6e18ff9e6c7f63d4d23ab3553a22dadabb1f2ce3
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-09-12 06:01:05 +0900

    0020_ValidParentheses.pyの修正

--- 0020_ValidParentheses.py
+++ 0020_ValidParentheses.py
@@ -1,21 +1,18 @@
-def is_valid(s: str) -> bool:
-    stack = []
-    bracket_map = {')': '(', '}': '{', ']': '['}
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        bracket_map = {')': '(', '}': '{', ']': '['}
 
-    for char in s:
-        if char in bracket_map:
-            if stack:
-                top_element = stack.pop()
+        for char in s:
+            if char not in bracket_map:
+                stack.append(char)
             else:
-                top_element = '#'
+                top_element = stack.pop() if stack else '#'
 
-            if top_element != bracket_map[char]:
-                return False
+                if top_element != bracket_map[char]:
+                    return False
 
-        else:
-            stack.append(char)
-
-    return not stack
+        return not stack
 
 
 print(is_valid("()"))      # True
