17b66a92e156d1cf58af0c1ddb06fb43f43d7391
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-09-13 05:35:04 +0900

    0387_FirstUniqueCharacterinaString2.py

---
+++ 0387_FirstUniqueCharacterinaString2.py
@@ -0,0 +1,21 @@
+from collections import Counter
+
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        if not s:
+            return -1
+
+        count = Counter(s)
+
+        for index, char in enumerate(s):
+            if count[char] == 1:
+                return index
+
+        return -1
+
+
+# テストケース
+s = "leetcode"
+solution = Solution()
+print(solution.firstUniqChar(s))  # 出力: 0
