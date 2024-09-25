3dd51c58e8e5777806cc6e45e04800a95c415869
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-09-26 03:38:54 +0900

    0746_MinCostClimbingStairs.pyの修正

--- 0746_MinCostClimbingStairs.py
+++ 0746_MinCostClimbingStairs.py
@@ -10,7 +10,7 @@ class Solution:
             dp[i] = min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2])
 
         print(dp)
-        return dp
+        return dp[-1]
 
 
 cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
