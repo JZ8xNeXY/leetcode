9e55ada464e08607f0947af40f7c73b2c6e91103
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-09-29 06:51:03 +0900

    0072_EditDistanse.py

---
+++ 0072_EditDistanse.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        len1, len2 = len(word1), len(word2)
+
+        dp = [[0] * (len2 + 1) for _ in range(len1 + 1)]
+
+        for i in range(1, len1 + 1):
+            dp[i][0] = i
+        for j in range(1, len2 + 1):
+            dp[0][j] = j
+
+        for i in range(1, len1 + 1):
+            for j in range(1, len2 + 1):
+                if word1[i - 1] == word2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = min(dp[i - 1][j] + 1, dp[i]
+                                   [j - 1] + 1, dp[i - 1][j - 1] + 1)
+
+        print(dp)
+        return dp[len1][len2]
+
+
+# 実行例
+word1 = "horse"
+word2 = "ros"
+solution = Solution()
+print(solution.minDistance(word1, word2))
