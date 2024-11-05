f26f1a5f7c94cfc7cbf54b99ea91d16cc4eb8889
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-11-06 05:25:41 +0900

    0208_ImplementTrie.py

---
+++ 0208_ImplementTrie.py
@@ -0,0 +1,45 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}  # 子ノードを辞書で管理
+        self.is_end_of_word = False  # 単語の終端を示すフラグな
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end_of_word = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.is_end_of_word
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
+
+        # Your Trie object will be instantiated and called as such:
+trie = Trie()
+trie.insert("apple")
+print(trie.search("apple"))  # 出力: True
+print(trie.search("app"))    # 出力: False
+print(trie.startsWith("app"))  # 出力: True
+trie.insert("app")
+print(trie.search("app"))    # 出力: True
