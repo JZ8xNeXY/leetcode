564f4fa28c956004f6f765553a11c62b1a3a59d2
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-11-15 08:31:29 +0900

    0128_LongestConsecutiveSequence.pyの修正

--- 0128_LongestConsecutiveSequence.py
+++ 0128_LongestConsecutiveSequence.py
@@ -3,22 +3,21 @@ from typing import List
 
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
+        num_set = set(nums)
+        longest_streak = 0
 
-        sorted_nums = sorted(set(nums))
-        max_length = 1
-        current_length = 1
+        for num in num_set:
+            if num - 1 not in num_set:
+                current_num = num
+                current_streak = 1
 
-        for i in range(1, len(sorted_nums)):
-            if sorted_nums[i] == sorted_nums[i - 1] + 1:
-                current_length += 1
-            else:
-                max_length = max(max_length, current_length)
-                current_length = 1
+                while current_num + 1 in num_set:
+                    current_num += 1
+                    current_streak += 1
 
-        max_length = max(max_length, current_length)
-        return max_length
+                longest_streak = max(longest_streak, current_streak)
+
+        return longest_streak
 
 
 nums = [100, 4, 200, 1, 3, 2]
