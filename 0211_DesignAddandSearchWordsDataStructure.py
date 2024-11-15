f05f726579201862e852a1da1aba7eb8d740f623
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-11-16 06:45:51 +0900

    0211_DesignAddandSearchWordsDataStructure.pyの修正

--- 0211_DesignAddandSearchWordsDataStructure.py
+++ 0211_DesignAddandSearchWordsDataStructure.py
@@ -1,7 +1,5 @@
 class TrieNode:
     def __init__(self):
-        # self.children = {}
-        # self.is_end_of_word = False
         self.children = {}
         self.is_end_of_word = False
 
@@ -9,17 +7,9 @@ class TrieNode:
 class WordDictionary:
 
     def __init__(self):
-        # self.root = TrieNode()
         self.root = TrieNode()
 
     def addWord(self, word: str) -> None:
-        # node = self.root
-        # for char in word:
-        #     if char not in node.children:
-        #         node.children[char] = TrieNode()
-        #     node = node.children[char]
-        # node.is_end_of_word = True
-
         node = self.root
 
         for char in word:
@@ -29,33 +19,19 @@ class WordDictionary:
         node.is_end_of_word = True
 
     def search(self, word: str) -> bool:
-        # def dfs(j, node):
-        #     for i in range(j, len(word)):
-        #         char = word[i]
-
-        #         if char == '.':
-        #             return any(dfs(i + 1, child) for child in node.children.values())
-
-        #         elif char in node.children:
-        #             node = node.children[char]
-        #         else:
-        #             return False
-        #     return node.is_end_of_word
-
-        # return dfs(0, self.root)
-
         def dfs(j, node):
             for i in range(j, len(word)):
                 char = word[i]
 
                 if char == '.':
-                    return any(dfs(i+1, child) for child in node.children.values())
+                    return any(dfs(i + 1, child) for child in node.children.values())
                 elif char in node.children:
                     node = node.children[char]
                 else:
                     return False
+            return node.is_end_of_word
 
-        dfs(0, self.root)
+        return dfs(0, self.root)
 
 
 obj = WordDictionary()
