cbf21465c77aa04fe6b7b3d6b482527c3567665c
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-11-22 12:57:23 +0900

    0090_SubsetII.py

---
+++ 0090_SubsetII.py
@@ -0,0 +1,32 @@
+from typing import List
+
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result = []
+
+        def backtrack(start, path):
+
+            result.append(list(path))
+
+            for i in range(start, len(nums)):
+                if i > start and nums[i] == nums[i-1]:
+                    continue
+
+                path.append(nums[i])
+
+                backtrack(i+1, path)
+
+                path.pop()
+
+            return result
+
+        backtrack(0, [])
+
+        return result
+
+
+solution = Solution()
+nums = [1, 2, 2]
+print(solution.subsetsWithDup(nums))
