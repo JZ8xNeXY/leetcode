79add6eb0acae31ff21da7c4f5a05467319d097d
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-11-30 07:09:28 +0900

    0763_Partition.py

---
+++ 0763_Partition.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        last_position = {}
+        for index, char in enumerate(s):
+            last_position[char] = index
+
+        partions = []
+        start, end = 0, 0
+
+        for index, char in enumerate(s):
+            end = max(end, last_position[char])
+            if index == end:
+                partions.append(end - start + 1)
+                start = index + 1
+        return partions
+
+
+s = "ababcbacadefegdehijhklij"
+solution = Solution()
+print(solution.partitionLabels(s))
