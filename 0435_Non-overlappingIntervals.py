843873efe01bc11f4e724b87b595231ef90d2b92
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-12-07 08:49:32 +0900

    0435_Non-overlappingIntervals.py

---
+++ 0435_Non-overlappingIntervals.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def eraseOverlapIntervals(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: int
+        """
+
+        n = len(intervals)
+
+        intervals.sort(key=lambda x: x[1])
+        non_overlap_count = 0
+
+        end = float('-inf')
+
+        for interval in intervals:
+            if interval[0] >= end:
+                end = interval[1]
+                non_overlap_count += 1
+
+        return n - non_overlap_count
+
+
+intervals = [[1, 2], [1, 3], [2, 3], [3, 4]]
+solution = Solution()
+print(solution.eraseOverlapIntervals(intervals))
