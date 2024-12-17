14f493873d1c02ea4fb0f5e6f3b26258a0fffb57
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-12-18 07:51:07 +0900

    0844_BackspaceStringCompare.py

---
+++ 0844_BackspaceStringCompare.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def backspaceCompare(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        def build_string(string):
+            stack = []
+            for char in string:
+                if char == '#':
+                    if stack:
+                        stack.pop()
+                else:
+                    stack.append(char)
+            return ''.join(stack)
+
+        return build_string(s) == build_string(t)
+
+
+# テストケース
+s = "xywrrmp"
+t = "xywrrmu#p"
+solution = Solution()
+print(solution.backspaceCompare(s, t))
