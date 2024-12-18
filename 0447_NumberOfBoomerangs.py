7fb03ea2d73648c1137df66e6dc8666e8d9f1257
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-12-19 05:17:02 +0900

    0447_NumberOfBoomerangs.py

---
+++ 0447_NumberOfBoomerangs.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+import math
+
+
+class Solution(object):
+    def numberOfBoomerangs(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        result = 0
+
+        for i in points:
+            distance_count = defaultdict(int)
+
+            for j in points:
+                if i == j:
+                    continue
+
+                distance = (i[0] - j[0]) ** 2 + (i[1]-j[1]) ** 2
+                distance_count[distance] += 1
+
+            for count in distance_count.values():
+                result += count * (count - 1)
+
+        return result
+
+
+points = [[0, 0], [1, 0], [2, 0]]
+soluiton = Solution()
+print(soluiton.numberOfBoomerangs(points))
