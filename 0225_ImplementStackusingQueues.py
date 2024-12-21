a9859a3e39c7c5f5851083369f9698d6d80e84e2
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-12-21 18:56:19 +0900

    0225_ImplementStackusingQueues.py

---
+++ 0225_ImplementStackusingQueues.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+
+class MyStack(object):
+
+    def __init__(self):
+        self.queue1 = deque()
+        self.queue2 = deque()
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.queue2.append(x)
+        while self.queue1:
+            self.queue2.append(self.queue1.popleft())
+        self.queue1, self.queue2 = self.queue2, self.queue1
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+        return self.queue1.popleft()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.queue1[0]
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+        return not self.queue1
+
+
+myStack = new MyStack()
+myStack.push(1)
+myStack.push(2)
+myStack.top()
+myStack.pop()
+myStack.empty()
