a4284b81623fb03dd061690194e169061bea7459
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-12-26 05:34:10 +0900

    0309_BestTimetoBuyandSellStockwithCooldown.py

---
+++ 0309_BestTimetoBuyandSellStockwithCooldown.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if not prices:
+            return 0
+        n = len(prices)
+        hold = [0] * n
+        sell = [0] * n
+        cooldown = [0] * n
+
+        hold[0] = -prices[0]
+        sell[0] = 0
+        cooldown[0] = 0
+
+        for i in range(1, n):
+            hold[i] = max(hold[i-1], cooldown[i-1] - prices[i])
+            sell[i] = hold[i-1] + prices[i]
+            cooldown[i] = max(cooldown[i-1], sell[i-1])
+
+        return max(cooldown[n-1], sell[n-1])
+
+
+prices = [1, 2, 3, 0, 2]
+solution = Solution()
+print(solution.maxProfit(prices))  # 3
