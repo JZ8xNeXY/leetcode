3f809a4dbc4df971a1096ce53a9b3f6b7e5b4153
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2025-01-09 05:03:47 +0900

    0242_ValidAnagrams.pyの修正

--- 0242_ValidAnagrams.py
+++ 0242_ValidAnagrams.py
@@ -4,10 +4,3 @@ from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return Counter(s.lower()) == Counter(t.lower())
-
-
-solution = Solution()
-s = "anagram",
-t = "nagaram"
-
-print(solution.isAnagram(s, t))
