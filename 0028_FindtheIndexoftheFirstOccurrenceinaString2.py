0c9b4d2e07390d3e0c4af7f609fe4d0ff0b1c9c2
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2025-01-09 05:18:41 +0900

    0028_FindtheIndexoftheFirstOccurrenceinaString2.pyの修正

--- 0028_FindtheIndexoftheFirstOccurrenceinaString2.py
+++ 0028_FindtheIndexoftheFirstOccurrenceinaString2.py
@@ -1,9 +1,3 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         return haystack.find(needle)
-
-
-haystack = "leetcode"
-needle = "leet"
-solution = Solution()
-print(solution.strStr(haystack, needle))
