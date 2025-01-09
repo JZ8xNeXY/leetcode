79617ee99a05edd06652b07ae8e86f8dfc00d032
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2025-01-10 05:41:10 +0900

    0125_ValidPalindrome.pyの修正

--- 0125_ValidPalindrome.py
+++ 0125_ValidPalindrome.py
@@ -2,7 +2,3 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         filtered_chars = [char.lower() for char in s if char.isalnum()]
         return filtered_chars == filtered_chars[::-1]
-
-
-solution = Solution()
-print(solution.isPalindrome("A man, a plan, a canal: Panama"))
