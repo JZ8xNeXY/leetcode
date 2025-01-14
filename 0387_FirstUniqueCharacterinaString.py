d64368a93738a0779a09fc716e689a20b9a040b2
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2025-01-15 05:39:17 +0900

    0387_FirstUniqueCharacterinaString.pyの修正

--- 0387_FirstUniqueCharacterinaString.py
+++ 0387_FirstUniqueCharacterinaString.py
@@ -3,20 +3,6 @@ class Solution:
         if not s:
             return -1
 
-        # duplicate = set()
-        # position = {}
-
-        # for index, char in enumerate(s):
-        #     if char not in position:
-        #         position[char] = index
-        #     else:
-        #         duplicate.add(char)
-
-        # for char in s:
-        #     if char not in duplicate:
-        #         return position[char]
-
-        # return -1
         duplicate = set()
         position = {}
 
@@ -31,8 +17,3 @@ class Solution:
                 return position[char]
 
         return -1
-
-
-s = "leetcode"
-solution = Solution()
-print(solution.firstUniqChar(s))
