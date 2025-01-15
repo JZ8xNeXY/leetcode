7c89656c5a79bf7642b2a4b321fd42b307a857d3
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2025-01-16 05:15:00 +0900

    0049_GroupAnagrams.pyの修正

--- 0049_GroupAnagrams.py
+++ 0049_GroupAnagrams.py
@@ -10,10 +10,4 @@ class Solution:
             sorted_str = ''.join(sorted(s))
             anagrams[sorted_str].append(s)
 
-        print(list(anagrams.values()))
         return list(anagrams.values())
-
-
-strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
-solution = Solution()
-solution.groupAnagrams(strs)
