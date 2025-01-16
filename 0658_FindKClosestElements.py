6edceb4a819ebec8ebc496636a72222521375d55
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2025-01-17 05:18:50 +0900

    0658_FindKClosestElements.pyの修正

--- 0658_FindKClosestElements.py
+++ 0658_FindKClosestElements.py
@@ -10,19 +10,9 @@ class Solution(object):
         while left < right:
             mid = (left + right) // 2
 
-            # if x - arr[mid]:現在の部分配列の左端からxまでの距離
-            # arr[mid + k] - x:次の部分配列の右端からxまでの距離
             if x - arr[mid] > arr[mid + k] - x:
                 left = mid + 1
             else:
                 right = mid
 
         return arr[left:left+k]
-
-
-arr = [1, 2, 3, 4, 5]
-k = 3
-x = 3
-
-solution = Solution()
-print(solution.findClosestElements(arr, k, x))
