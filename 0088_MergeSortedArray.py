2b24a9678998f2a61e11eb10ac39c9335966aae6
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2025-01-17 04:54:27 +0900

    0088_MergeSortedArray.pyの修正

--- 0088_MergeSortedArray.py
+++ 0088_MergeSortedArray.py
@@ -26,14 +26,4 @@ class Solution:
             j -= 1
             k -= 1
 
-        print(nums1)
         return nums1
-
-
-nums1 = [1, 2, 3, 0, 0, 0]
-m = 3
-nums2 = [2, 5, 6]
-n = 3
-
-solution = Solution()
-solution.merge(nums1, m, nums2, n)
