4c71dd0d9c1f9c6b43ac6cbda8f583623d248e74
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2025-01-18 05:12:58 +0900

    0703_KthLargestElementinaStream.pyの修正

--- 0703_KthLargestElementinaStream.py
+++ 0703_KthLargestElementinaStream.py
@@ -8,10 +8,8 @@ class KthLargest:
         self.k = k
         self.heap = nums
 
-        # リストをヒープに変換
         heapq.heapify(self.heap)
 
-        # ヒープを作成しサイズを制限
         while len(self.heap) > k:
             heapq.heappop(self.heap)
 
@@ -21,17 +19,4 @@ class KthLargest:
         if len(self.heap) > self.k:
             heapq.heappop(self.heap)
 
-        print(self.heap[0])
         return self.heap[0]
-
-
-kthLargest = KthLargest(3, [4, 5, 8, 2])
-kthLargest.add(3)
-
-kthLargest.add(5)
-
-kthLargest.add(10)
-
-kthLargest.add(9)
-
-kthLargest.add(4)
