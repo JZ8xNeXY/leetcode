edcd354acfb8f675b6bfcd1ca2601d3136aaef58
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2025-01-18 05:09:21 +0900

    0240_Searcha2DMatrixII.pyの修正

--- 0240_Searcha2DMatrixII.py
+++ 0240_Searcha2DMatrixII.py
@@ -20,14 +20,3 @@ class Solution(object):
                 row += 1
 
         return False
-
-
-# matrix = [[1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [
-#     3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]]
-# target = 5
-
-matrix = [[-1, 3]]
-target = 3
-
-solution = Solution()
-print(solution.searchMatrix(matrix, target))
