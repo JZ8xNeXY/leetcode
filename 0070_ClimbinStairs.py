dff5638c021d5865290b02b70b5df17d24ac05a9
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2025-01-21 05:30:03 +0900

    0070_ClimbinStairs.pyの修正

--- 0070_ClimbinStairs.py
+++ 0070_ClimbinStairs.py
@@ -1,20 +1,21 @@
 class Solution:
-    def climbStairs(self, n: int, memo={}) -> int:
-        if n in memo:
-            return memo[n]
-
+    def climbStairs(self, n: int) -> int:
         if n == 1:
             return 1
-
         if n == 2:
             return 2
 
-        memo[n] = self.climbStairs(n-1, memo) + self.climbStairs(n-2, memo)
+        dp = [0] * (n + 1)
 
-        print(memo)
-        return memo[n]
+        dp[1] = 1
+        dp[2] = 2
+
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+        return dp[n]
 
 
 solution = Solution()
 
-solution.climbStairs(15)
+print(solution.climbStairs(4))
