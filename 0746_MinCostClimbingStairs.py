210650b435160861be5da4b6e63f25912a57d374
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2025-01-22 05:18:54 +0900

    0746_MinCostClimbingStairs.py

--- 0746_MinCostClimbingStairs.py
+++ 0746_MinCostClimbingStairs.py
@@ -9,10 +9,4 @@ class Solution:
         for i in range(2, n+1):
             dp[i] = min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2])
 
-        print(dp)
         return dp[-1]
-
-
-cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
-solution = Solution()
-print(solution.minCostClimbingStairs(cost))
