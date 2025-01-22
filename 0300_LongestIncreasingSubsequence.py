3fc70484876c9c67644ec105f0124ffd58cc5aad
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2025-01-23 05:10:01 +0900

    0300_LongestIncreasingSubsequence.pyの修正

--- 0300_LongestIncreasingSubsequence.py
+++ 0300_LongestIncreasingSubsequence.py
@@ -14,8 +14,3 @@ class Solution:
                     dp[i] = max(dp[i], dp[j] + 1)
 
         return max(dp)
-
-
-nums = [10, 11, 2, 5, 3, 7, 101, 18]
-solution = Solution()
-print(solution.lengthOfLIS(nums))
