92254480815006363659402a5dffa084854ea6b9
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2025-01-24 05:12:43 +0900

    1976_NumberofWaystoArriveatDestination.pyの修正

--- 1976_NumberofWaystoArriveatDestination.py
+++ 1976_NumberofWaystoArriveatDestination.py
@@ -4,8 +4,6 @@ from collections import defaultdict
 
 MOD = 10**9 + 7
 
-# 最短距離の経路数を数える。今回なら距離7の経路数を計算する。
-
 
 class Solution:
     def countPaths(self, n: int, roads: List[List[int]]) -> int:
@@ -33,23 +31,14 @@ class Solution:
             for neighbor, next_dist in graph[node]:
                 new_dist = current_dist + next_dist
 
-                # 距離の管理
                 if new_dist < dist[neighbor]:
                     dist[neighbor] = new_dist
                     ways[neighbor] = ways[node]
 
                     heapq.heappush(queue, (neighbor, new_dist))
 
-                # 経路数の管理
                 elif new_dist == dist[neighbor]:
 
                     ways[neighbor] = (ways[neighbor] + ways[node]) % MOD
 
         return ways[n-1]
-
-
-n = 7
-roads = [[0, 6, 7], [0, 1, 2], [1, 2, 3], [1, 3, 3], [6, 3, 3],
-         [3, 5, 1], [6, 5, 1], [2, 5, 1], [0, 4, 5], [4, 6, 2]]
-solution = Solution()
-print(solution.countPaths(n, roads))
