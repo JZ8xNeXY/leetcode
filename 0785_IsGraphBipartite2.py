7efacea9e3d13eefc163a6a18cdb2c2d9bd42847
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2025-01-25 08:16:16 +0900

    0785_IsGraphBipartite2.py

---
+++ 0785_IsGraphBipartite2.py
@@ -0,0 +1,25 @@
+from typing import List
+from collections import deque
+
+
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        n = len(graph)
+        color = [0] * n
+
+        def dfs(node, current_color):
+            color[node] = current_color
+            for neighbor in graph[node]:
+                if color[neighbor] == 0:
+                    if not dfs(neighbor, -current_color):
+                        return False
+                elif color[neighbor] == color:
+                    return False
+            return True
+
+        for node in range(n):
+            if color[node] == 0:
+                if not dfs(node, 1):
+                    return False
+
+        return True
