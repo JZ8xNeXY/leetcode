1461a0c7e4cdf1013fd4bed1bd50272973258b10
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2025-01-26 08:47:53 +0900

    0841_KeysandRooms.pyの修正

--- 0841_KeysandRooms.py
+++ 0841_KeysandRooms.py
@@ -4,53 +4,19 @@ from typing import List
 # 深さ優先（DFS)
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        # n = len(rooms)
-
-        # visited = [False] * n
-
-        # stack = [0]
-
-        # visited[0] = True
-
-        # while stack:
-        #     current_room_key = stack.pop()
-        #     for key in rooms[current_room_key]:
-        #         if not visited[key]:
-        #             visited[key] = True
-        #             stack.append(key)
-
-        # return all(visited)
-
-        # n = len(rooms)
-
-        # visited = [False] * n
-
-        # stack = [0]
-
-        # visited[0] = True
-
-        # while stack:
-        #     current_room_key = stack.pop()
-        #     for key in rooms[current_room_key]:
-        #         if not visited[key]:
-        #             visited[key] = True
-        #             stack.append(key)
-
-        # return all(visited)
         n = len(rooms)
 
         visited = [False] * n
-
-        stack = [0]
-
         visited[0] = True
 
-        while stack:
-            current_room_key = stack.pop()
-            for key in rooms[current_room_key]:
-                if not visited[key]:
-                    visited[key] = True
-                    stack.append(key)
+        queue = deque([0])
+
+        while queue:
+            current_room_key = queue.popleft()
+            for neighbor in rooms[current_room_key]:
+                if not visited[neighbor]:
+                    visited[neighbor] = True
+                    queue.append(neighbor)
 
         return all(visited)
 
