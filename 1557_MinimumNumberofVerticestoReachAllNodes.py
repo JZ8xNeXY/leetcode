09957c28235eda8d6650d84c744755550763fb72
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2025-01-26 08:58:30 +0900

    1557_MinimumNumberofVerticestoReachAllNodes.pyの修正

--- 1557_MinimumNumberofVerticestoReachAllNodes.py
+++ 1557_MinimumNumberofVerticestoReachAllNodes.py
@@ -3,21 +3,15 @@ from typing import List
 
 class Solution:
     def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
-        count = [0] * n
+        in_degree = [0] * n
 
         for _, v in edges:
-            count[v] += 1
+            in_degree[v] += 1
 
         result = []
 
         for i in range(n):
-            if count[i] == 0:
+            if in_degree[i] == 0:
                 result.append(i)
 
         return result
-
-
-n = 6
-edges = [[0, 1], [0, 2], [2, 5], [3, 4], [4, 2]]
-solution = Solution()
-print(solution.findSmallestSetOfVertices(n, edges))
