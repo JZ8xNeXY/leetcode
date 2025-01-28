2cb4e01a3203f48ba51359c37402d5e1943ab4bb
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2025-01-29 05:12:45 +0900

    0056_MergeIntervals.pyの修正

--- 0056_MergeIntervals.py
+++ 0056_MergeIntervals.py
@@ -3,21 +3,6 @@ from typing import List
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # if not intervals:
-        #     return []
-
-        # intervals.sort(key=lambda x: x[0])
-
-        # merged = []
-
-        # for interval in intervals:
-        #     if not merged or merged[-1][1] < interval[0]:
-        #         merged.append(interval)
-        #     else:
-        #         merged[-1][1] = max(merged[-1][1], interval[1])
-
-        # return merged
-
         if not intervals:
             return []
 
@@ -32,8 +17,3 @@ class Solution:
                 merged[-1][1] = max(merged[-1][1], interval[1])
 
         return merged
-
-
-intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]
-solution = Solution()
-print(solution.merge(intervals))
