e83777047bbe00ddcfc8ef804b3f2b5beccec3d4
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2025-01-30 05:32:39 +0900

    0406_QueueReconstructionbyHeight.pyの修正

--- 0406_QueueReconstructionbyHeight.py
+++ 0406_QueueReconstructionbyHeight.py
@@ -5,8 +5,8 @@ class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
         people.sort(key=lambda x: (-x[0], x[1]))
 
-        queue = []
+        result = []
         for person in people:
-            queue.insert(person[1], person)
+            result.insert(person[1], person)
 
-        return queue
+        return result
