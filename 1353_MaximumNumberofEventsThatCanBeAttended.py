89eab4ccd53bb18c6e3b4c8f733ef0964f54b6ff
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2025-01-31 08:44:44 +0900

    1353_MaximumNumberofEventsThatCanBeAttended.pyの修正

--- 1353_MaximumNumberofEventsThatCanBeAttended.py
+++ 1353_MaximumNumberofEventsThatCanBeAttended.py
@@ -4,7 +4,7 @@ from typing import List
 
 class Solution:
     def maxEvents(self, events: List[List[int]]) -> int:
-        events.sort(key=lambda x: x[0])
+        events.sort()
         min_heap = []
         event_index = 0
         attended_events = 0
@@ -18,16 +18,12 @@ class Solution:
                 heapq.heappush(min_heap, events[event_index][1])
                 event_index += 1
 
-            heapq.heappop(min_heap)
-            attended_events += 1
-            current_day += 1
+            if min_heap:
+                heapq.heappop(min_heap)
+                attended_events += 1
+                current_day += 1
 
             while min_heap and min_heap[0] < current_day:
                 heapq.heappop(min_heap)
 
         return attended_events
-
-
-events = [[1, 2], [2, 3], [3, 4]]
-solution = Solution()
-print(solution.maxEvents(events))
