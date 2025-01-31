1922d063229470d484834ce6e5cd6550e52576d3
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2025-02-01 08:17:50 +0900

    0787_CheapestFlightsWithinKStops.pyの修正

--- 0787_CheapestFlightsWithinKStops.py
+++ 0787_CheapestFlightsWithinKStops.py
@@ -16,32 +16,25 @@ class Solution(object):
         for u, v, w in flights:
             graph[u].append((v, w))
 
-        # (cost, node, stops)
-        heap = [(0, src, 0)]
-
         dist = [[float('inf')] * (k + 2) for _ in range(n)]
         dist[src][0] = 0
 
+        heap = [(0, src, 0)]
+
         while heap:
-            current_cost, node, stops = heapq.heappop(heap)
-            if node == dst:
+            current_cost, city, transfer_cost = heapq.heappop(heap)
+
+            if city == dst:
                 return current_cost
-            if stops > k:
+
+            if transfer_cost > k:
                 continue
-            for neighbor, next_cost in graph[node]:
+
+            for neighbor, next_cost in graph[city]:
                 new_cost = current_cost + next_cost
-                if new_cost < dist[neighbor][stops+1]:
-                    dist[neighbor][stops+1] = new_cost
-                    heapq.heappush(heap, (new_cost, neighbor, stops+1))
+
+                if new_cost < dist[neighbor][transfer_cost+1]:
+                    dist[neighbor][transfer_cost+1] = new_cost
+                    heapq.heappush(heap, (new_cost, neighbor, transfer_cost+1))
 
         return -1
-
-
-n = 4
-flights = [[0, 1, 100], [1, 2, 100], [2, 0, 100], [1, 3, 600], [2, 3, 200]]
-src = 0
-dst = 3
-k = 1
-
-solution = Solution()
-print(solution.findCheapestPrice(n, flights, src, dst, k))
