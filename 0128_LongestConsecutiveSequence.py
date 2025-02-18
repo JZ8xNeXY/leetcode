8aa0a3fe65de2911a5aa2c32035db12540b04238
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2025-02-19 05:08:11 +0900

    0128_LongestConsecutiveSequence.pyの修正

--- 0128_LongestConsecutiveSequence.py
+++ 0128_LongestConsecutiveSequence.py
@@ -9,6 +9,7 @@ class Solution:
         for num in num_set:
             if num - 1 not in num_set:
                 current_num = num
+                current_streak = 0
                 current_streak = 1
 
                 while current_num + 1 in num_set:
@@ -18,8 +19,3 @@ class Solution:
                 longest_streak = max(longest_streak, current_streak)
 
         return longest_streak
-
-
-nums = [100, 4, 200, 1, 3, 2]
-solution = Solution()
-print(solution.longestConsecutive(nums))
